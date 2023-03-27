02796a605b11df4de37a8ab0833a8cd8b65d8520
Qarty26 <116076301+Qarty26@users.noreply.github.com>
2023-03-27 13:23:42 +0300

    Update main.cpp

--- Lab/Homework 2/main.cpp
+++ Lab/Homework 2/main.cpp
@@ -6,16 +6,34 @@ ifstream in("date.in");
 ofstream out("date.out");
 
 
+
 int lungimeMax,stareFinala[100],nrStariFinale;
+bool fout;
 
 void afisare(char M[100][100][10],int nrStari)
 {
+    cout<<endl;
     for(int i=0;i<nrStari;i++)
     {
         for(int j=0;j<nrStari;j++)
             cout<<M[i][j]<<" ";
         cout<<endl;
     }
+    cout<<endl;
+}
+
+void afisDate(int nrStari,int nrMuchii,int stareInitiala, int nrStariFinale, int stareFinala[100],int lungimeMax)
+{
+    cout<<endl;
+    cout<<"Numar de stari: "<<nrStari<<endl;
+    cout<<"Numar de muchii: "<<nrMuchii<<endl;
+    cout<<"Stare initiala: "<<stareInitiala<<endl;
+    cout<<"Numar de stari finale: "<<nrStariFinale<<endl;
+    cout<<"Stari finale: ";
+    for(int i=0;i<nrStariFinale;i++)
+        cout<<stareFinala[i]<<" ";
+    cout<<endl;
+    cout<<"Lungimea maxima admisa: "<<lungimeMax<<endl<<endl;
 }
 
 void cautare(char M[100][100][10],int nrStari,int lungimeActuala,int stareActuala,char cuv[100])
@@ -23,6 +41,14 @@ void cautare(char M[100][100][10],int nrStari,int lungimeActuala,int stareActual
     for(int i=0;i<nrStariFinale;i++)
         if(stareActuala==stareFinala[i])
         {
+
+        if(fout==false)
+        {
+            cout<<cuv<<endl;
+            break;
+        }
+
+        else
             out<<cuv<<endl;
             break;
         }
@@ -49,54 +75,153 @@ void cautare(char M[100][100][10],int nrStari,int lungimeActuala,int stareActual
 
 int main()
 {
-
     int nrStari,stareInitiala,nrMuchii,st,dr,i,j;
     char M[100][100][10],val[100];
+    bool fin,inMenu=true,inSubMenu=true;
+    int choice;
 
-    //cout<<"Nr stari: ";
-    in>>nrStari;
-    ///citire nr stari
-    for(i=0;i<nrStari;i++)
-        for(j=0;j<nrStari;j++)
-            strcpy(M[i][j],"#");
 
-    //cout<<"Nr muchii: ";
-    in>>nrMuchii;
-    ///citire nr muchii
-
-    ///citire muchii+compl matrice adiacenta
-    //cout<<"Muchii:"<<endl;
-    for(i=0;i<nrMuchii;i++)
+    ///MENIU_________________________________________________________________________________
+    while(inMenu==true)
     {
-        in>>st>>dr>>val;
-        if(strcmp(M[st][dr],"#")==0)
-            strcpy(M[st][dr],val);
-        else{
-            strcat(M[st][dr],val);
+        cout<<"From where do you want to read data? 0->Console  1->File"<<endl;
+        cin>>fin;
+
+        cout<<"Where do you want to print data? 0->Console  1->File"<<endl;
+        cin>>fout;
+
+        ///citire nr stari------------------------------------------------------------------
+        if(fin==false){
+            cout<<"Nr stari: ";
+            cin>>nrStari;
         }
-    }
-    //cout<<"Stare initiala: ";
-    in>>stareInitiala;
-    ///citire stare initiala
-
-    //cout<<"Nr stari finale: ";
-    in>>nrStariFinale;
-
-    //cout<<"Stari finale:"<<endl;
-    for(i=0;i<nrStariFinale;i++)
-        in>>stareFinala[i];
-    ///citire stari finale
-
-    //cout<<"Lungime maxima: ";
-    in>>lungimeMax;
-    ///citire lungime maxima
-
-    char cuv[lungimeMax+1];
-    for(i=0;i<lungimeMax;i++)
-        cuv[i]=' ';
+        else{
+            in>>nrStari;
+        }
 
 
-    cautare(M,nrStari,0,stareInitiala,cuv);
+
+        for(i=0;i<nrStari;i++)
+            for(j=0;j<nrStari;j++)
+                strcpy(M[i][j],"#");
+
+
+        ///citire nr muchii-------------------------------------------------------------------
+        if(fin==false){
+            cout<<"Nr muchii: ";
+            cin>>nrMuchii;
+        }
+        else{
+            in>>nrMuchii;
+        }
+
+        ///citire muchii+compl matrice adiacenta------------------------------------------------
+        for(i=0;i<nrMuchii;i++)
+        {
+
+            if(fin==false)
+                cin>>st>>dr>>val;
+            else
+                in>>st>>dr>>val;
+
+
+
+            if(strcmp(M[st][dr],"#")==0)
+                strcpy(M[st][dr],val);
+            else
+                strcat(M[st][dr],val);
+
+        }
+
+
+        ///citire stare initiala-------------------------------------------------------------------
+        if(fin==false){
+            cout<<"Stare initiala: ";
+            cin>>stareInitiala;
+        }
+        else{
+            in>>stareInitiala;
+        }
+
+
+        ///citire nr stare finala---------------------------------------------------------------------
+        if(fin==false){
+            cout<<"Nr stari finale: ";
+            cin>>nrStariFinale;
+        }
+        else{
+            in>>nrStariFinale;
+        }
+
+
+        ///citire stari finale-------------------------------------------------------------------------
+        for(i=0;i<nrStariFinale;i++)
+            if(fin==false){
+                cout<<"Stari finale: ";
+                cin>>stareFinala[i];
+            }
+            else{
+                in>>stareFinala[i];
+            }
+
+            ///citire len max-------------------------------------------------------------------------
+            if(fin==false){
+                cout<<"Lungime maxima: ";
+                cin>>lungimeMax;
+            }
+            else{
+                in>>lungimeMax;
+            }
+
+
+        char cuv[lungimeMax+1];
+        for(i=0;i<lungimeMax;i++)
+            cuv[i]=' ';
+
+        ///SUBMENIU_________________________________________________________________________________________
+        inSubMenu=true;
+        while(inSubMenu==true)
+        {
+
+            cout<<"Choose what to do..."<<endl;
+            cout<<"1->Find all the accepted paths"<<endl;
+            cout<<"2->Print the adjacency matrix"<<endl;
+            cout<<"3->Print the data"<<endl;
+            cout<<"Any other number->Leave"<<endl;
+            cin>>choice;
+            switch(choice)
+            {
+            case 1:
+                {
+                    cautare(M,nrStari,0,stareInitiala,cuv);
+                    break;
+                }
+            case 2:
+                {
+                    afisare(M,nrStari);
+                    break;
+                }
+            case 3:
+                {
+                    afisDate(nrStari,nrMuchii,stareInitiala,nrStariFinale,stareFinala,lungimeMax);
+                    break;
+                }
+            default:
+                {
+                    inSubMenu=false;
+                    break;
+                }
+            }
+        }
+
+        cout<<"Do you want to run again?"<<endl;
+        cout<<"0->No"<<endl;
+        cout<<"1->Yes"<<endl;
+        cin>>choice;
+
+        if(choice==0)
+            inMenu=false;
+        }
 
     return 0;
 }
