b5fa0213f3d4cf2eff8eb6522dad27cc21095424
Qarty26 <116076301+Qarty26@users.noreply.github.com>
2023-05-11 13:57:57 +0300

    Add files via upload

--- Lab/Homework 3/main.cpp
+++ Lab/Homework 3/main.cpp
@@ -10,26 +10,23 @@ class Gramatica
 {
 char litera;
 vector<vector<string>>sir;
-vector<string>productie;
 
 public:
 
     Gramatica()
     {
         sir.resize(100);
-        in>>litera;
-        int n;
-        string s;
-        in>>n;
-        for(int i=0;i<n;i++)
+        while(in>>litera)
         {
-            in>>s;
-            productie.push_back(s);
+            int n;
+            string s;
+            in>>n;
+            for(int i=0;i<n;i++)
+            {
+                in>>s;
+                sir[litera].push_back(s);
+            }
         }
-
-        sir[litera]=productie;
-
-
     }
 
     void afisare()
@@ -39,12 +36,39 @@ public:
         {
             if(sir[i].size()!=0)
             {
+                cout<<(char)i<<" -> ";
                 for(j=0;j<sir[i].size()-1;j++)
                     cout<<sir[i][j]<<"|";
                 cout<<sir[i][j]<<endl;
             }
 
         }
+    }
+
+    bool acceptare(char start, string cuvant, int index)
+    {
+        if(index==cuvant.size())
+        {
+            cout<<"aici ies"<<endl;
+            return true;
+        }
+
+
+        for(int i=0;i<sir[start].size();i++)
+            if(sir[start][i][0]==cuvant[index] && sir[start][i][1] && index<cuvant.size()-1)
+            {
+                cout<<"Movin from "<<(char)start<<" to "<<sir[start][i][1]<<" with "<<sir[start][i]<<endl;
+                return acceptare(sir[start][i][1],cuvant,index+1);
+            }
+
+            else if(sir[start][i][0]==cuvant[index] && !sir[start][i][1] && index==cuvant.size()-1)
+            {
+                cout<<"Terminating here with my nigga: "<<sir[start][i]<<endl;
+                return true;
+            }
+
+        cout<<"Am crapat"<<endl;
+        return false;
     }
 
 };
@@ -54,6 +78,7 @@ int main()
 
 Gramatica g;
 g.afisare();
+cout<<g.acceptare('S',"abccdxabccdd",0);
 
     return 0;
 }
